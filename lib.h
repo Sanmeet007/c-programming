f06396052ddc23abc9470a9211c7e777a01935b4
Sanmeet007 <ssanmeet123@gmail.com>
2022-11-07 13:48:58 +0530

    cleaned imports

---
+++ lib.h
@@ -0,0 +1,9 @@
+#ifndef LIB_HEADER_FILE
+#define LIB_HEADER_FILE
+
+#include "./library/functions.h"
+#include "./library/list.h"
+#include "./library/variant_list.h"
+#include "./library/range.h"
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
-#include <iostream>
-#include <variant>
-#include "list.h"
-#include "range.h"
-#include "functions.h"
-
-using std::cin;
-using std::cout;
-using std::string;
-
-void handler(int index, string x)
-{
-    cout << index << x << std::endl;
-}
+#include "lib.h"
 
 int main(int argc, char const *argv[])
 {
-    //
-    auto li = List<string>();
-    li.push("ONE");
-    li.push("TWO");
-    li.push("THREE");
-    li.print_values();
+    cout << "Hello world";
     return 0;
 }
